2caa8759152f62bae500a4b1973ed9e116056d88
ODIAN <omardiaa27@gmail.com>
2021-01-26 19:44:44 +0200

    first commit

---
+++ EmployeeManagement/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmployeeManagement.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Welcome(string name, int id)
+        {
+            ViewData["name"] = name;
+            ViewData["id"] = id;
+
+            return View();
+        }
+    }
+}
